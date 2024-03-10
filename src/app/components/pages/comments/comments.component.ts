642ee833bd3bb2265d40e6d0b992746e1c67a148
Ivan Matveev <berkut89@list.ru>
2024-03-11 10:40:37 +1200

    last comments

--- src/app/components/pages/comments/comments.component.ts
+++ src/app/components/pages/comments/comments.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../API/api.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MainService } from '../../../main.service';
 import { PostsComponent } from '../../ui/posts/posts.component';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { Location } from '@angular/common';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource, MatTreeModule} from '@angular/material/tree';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTreeNestedDataSource, MatTreeModule } from '@angular/material/tree';
+import { NestedTreeControl, CdkTreeModule } from '@angular/cdk/tree';
+import { ArrayDataSource, DataSource } from '@angular/cdk/collections';
 interface commentsNode {
   id: number;
   author: string;
@@ -22,35 +22,47 @@ interface commentsNode {
   children?: commentsNode[];
 }
 
-
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [HttpClientModule, PostsComponent, CommonModule, MatButtonModule, MatDividerModule, MatIconModule, MatTreeModule, MatButtonModule, MatIconModule, MatProgressBarModule],
+  imports: [HttpClientModule,
+    PostsComponent,
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTreeModule,
+    CdkTreeModule,
+    MatButtonModule,
+    MatProgressBarModule],
   providers: [ApiService, MainService],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss'
 })
-export class CommentsComponent {
-  currentURL:string = ''
+export class CommentsComponent implements OnInit {
+  currentURL!: string;
   lastWordUrl: any;
-  treeControl = new NestedTreeControl<commentsNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<commentsNode>();
-  dataArray: commentsNode[] = [];
-  data: any;
-  constructor(private service: ApiService, private location: Location) {
+  data!: any;
+  dataSource!: ArrayDataSource<commentsNode>;
+  treeControl!: NestedTreeControl<commentsNode>;
+  constructor(private service: ApiService, private location: Location, private changeDetectorRef: ChangeDetectorRef) { }
+  ngOnInit(): void {
     this.currentURL = window.location.href;
     this.lastWordUrl = this.currentURL.split('/').pop();
-    this.service.getDataComments(this.lastWordUrl).subscribe(response => { 
-      this.data = {...response}
-      //this.dataArray.push(...response as [])
-      this.dataSource.data = this.data;
+    this.service.getDataComments(this.lastWordUrl).subscribe(response => {
+      this.data = { ...response }
+      this.treeControl = new NestedTreeControl<commentsNode>(this.getChildren);
+      this.dataSource = new ArrayDataSource(this.data);
+      
       console.log(this.dataSource)
-      console.log(this.dataSource.data)
-    }); 
+    });
   }
+
   histroryBack() {
     this.location.back()
   }
+  getChildren = (node: commentsNode) => {
+    return node.children;
+  };
+  
   hasChild = (_: number, node: commentsNode) => !!node.children && node.children.length > 0;
 }
